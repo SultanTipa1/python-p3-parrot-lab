d0437816b468590e78432995defe34d4544a82c9
SultanTipa1 <josephngumbaugush@gmail.com>
2024-09-23 09:41:06 +0300

    Adds all files

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,21 +1,3 @@
-def parrot(message = "Squawk!"):
-    print(message)
-    return message
-
-
-
-# def appendItem(itemName, itemList[]):
-#     itemList.append(itemName)
-#     return itemList
-
-# print(appendItem('notebook'))
-# print(appendItem('pencil'))
-# print(appendItem('eraser'))
-
-# def add_item_to_dictionary(item_name, quantity, item_list =  {}):
-#     item_list[item_name] = quantity
-#     return item_list
-
-# print(add_item_to_dictionary('noteook', 4))
-# print(add_item_to_dictionary('pencil',1))
-
+def parrot(hello = "Squawk!"):
+    print(hello)
+    return hello
